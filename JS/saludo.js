f4091f1a1d86af7b60a1b8ab2826c18f3efa37f2
nabraham-sys <nabraham@udesa.edu.ar>
2026-06-11 16:08:54 -0300

    Saludo.js completo

---
+++ JS/saludo.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let email = localStorage.getItem("userEmail");
+
+    let sinEmail = document.querySelector(".sinEmail");
+    let conEmail = document.querySelector(".conEmail");
+    let saludo = document.querySelector(".saludo");
+    let logOut = document.querySelector(".logOut");
+
+    if (email === null) {
+        sinEmail.style.display = "block";
+        conEmail.style.display = "none";
+    } else if (email === "") {
+        sinEmail.style.display = "block";
+        conEmail.style.display = "none";
+    } else {
+        sinEmail.style.display = "none";
+        conEmail.style.display = "block";
+
+        saludo.textContent = "Bienvenido: " + email;
+    }
+});
