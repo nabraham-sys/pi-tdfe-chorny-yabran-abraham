9d07bbe305cc240a26455feb80bf50c433d34c82
fchorny-dh <fchorny@udesa.edu.ar>
2026-06-15 18:49:51 -0300

    corrijo el logout y nav para que home no se oculte al loguear

--- js/logout.js
+++ js/logout.js
@@ -1,8 +1,8 @@
 let menuLogin = document.querySelector(".sinEmail");
 let menuUsuario = document.querySelector(".conEmail");
-let logOut = document.querySelector(".logOut");
+let botonLogout = document.querySelector(".logOut");
 
-logOut.addEventListener("click", function () {
+botonLogout.addEventListener("click", function () {
     localStorage.removeItem("emailLogueado");
     menuLogin.style.display = "block";
     menuUsuario.style.display = "none";
